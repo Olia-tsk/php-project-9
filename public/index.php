5420b94ac212f6040fbc34c3019de1091a0eea7a
Ольга Курбатова <kon@2i.tusur.ru>
2025-03-27 16:21:50 +0700

    connect PhpRenderer and create container

--- public/index.php
+++ public/index.php
@@ -2,13 +2,21 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
-$app = AppFactory::create();
+$container = new Container();
+
+$container->set('renderer', function () {
+    return new PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Hello there');
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
 
 $app->run();
