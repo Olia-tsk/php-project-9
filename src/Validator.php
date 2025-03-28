d47d267d773f2df5ad6e7a249357f90571c05755
Ольга Курбатова <kon@2i.tusur.ru>
2025-03-28 16:46:04 +0700

    add Validator

---
+++ src/Validator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Analyzer;
+
+class Validator
+{
+    public function validate(array $urlData): array
+    {
+        $errors = [];
+        $pattern = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)';
+        if (empty($urlData['name'])) {
+            $errors['name'] = "URL не должен быть пустым";
+        } else if (!mb_ereg_match($pattern, $urlData['name'])) {
+            $errors['name'] = "Некорректный URL";
+        } else if (strlen($urlData['name']) > 255) {
+            $errors['name'] = "Слишком длинный URL";
+        }
+
+        return $errors;
+    }
+}
