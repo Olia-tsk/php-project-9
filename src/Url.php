1a5f070b373aa2fbd6f85fad9edf67beb5840da7
Ольга Курбатова <kon@2i.tusur.ru>
2025-04-01 16:34:44 +0700

    create class Url

---
+++ src/Url.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Analyzer;
+
+use Carbon\Carbon;
+
+class Url
+{
+    private ?int $id = null;
+    private ?string $name = null;
+    private ?string $created_at = null;
+
+    public static function fromArray(array $urlData): Url
+    {
+        $url = new Url();
+        $url->setName($urlData['name']);
+        $url->setDate(Carbon::now());
+        return $url;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->created_at;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setDate(string $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function exists(): bool
+    {
+        return !is_null($this->getId());
+    }
+}
