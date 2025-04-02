2a250b6854ef09248e41f19f658d40b2f4bdf972
Ольга Курбатова <kon@2i.tusur.ru>
2025-04-02 17:48:46 +0700

    add create url_checks table

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE IF NOT EXISTS urls (
     name VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url_id BIGINT REFERENCES urls (id),
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP NOT NULL
+);
