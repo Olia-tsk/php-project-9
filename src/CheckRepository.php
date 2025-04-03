d5f040a844c5a96c16438f297b2ed9e17e9878ab
Ольга Курбатова <kon@2i.tusur.ru>
2025-04-03 17:24:03 +0700

    edit CheckRepository class

--- src/CheckRepository.php
+++ src/CheckRepository.php
@@ -13,12 +13,13 @@ class CheckRepository
         $this->connection = $connection;
     }
 
-    public function addCheck($url_id): void
+    public function addCheck($url_id, $status_code): void
     {
-        $sql = "INSERT INTO url_checks (url_id, created_at) VALUES (:url_id, :created_at)";
+        $sql = "INSERT INTO url_checks (url_id, status_code, created_at) VALUES (:url_id, :status_code, :created_at)";
         $stmt = $this->connection->prepare($sql);
         $created_at = Carbon::now();
         $stmt->bindParam(':url_id', $url_id);
+        $stmt->bindParam(':status_code', $status_code);
         $stmt->bindParam(':created_at', $created_at);
         $stmt->execute();
     }
@@ -33,6 +34,7 @@ class CheckRepository
         while ($row = $stmt->fetch()) {
             $check['id'] = $row['id'];
             $check['url_id'] = $row['url_id'];
+            $check['status_code'] = $row['status_code'];
             $check['created_at'] = $row['created_at'];
             $checkData[] = $check;
         }
@@ -50,6 +52,7 @@ class CheckRepository
         if ($row = $stmt->fetch()) {
             $lastCheck['id'] = $row['id'];
             $lastCheck['url_id'] = $row['url_id'];
+            $lastCheck['status_code'] = $row['status_code'];
             $lastCheck['created_at'] = $row['created_at'];
         }
 
